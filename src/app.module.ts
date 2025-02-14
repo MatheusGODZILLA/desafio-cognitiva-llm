3d6c27eed482f3865d591bfd93dea86a8cfc3188
Matheus da Silva <contagodzillajapan@gmail.com>
2025-02-14 00:56:51 -0300

    Feat: adicionando recurso Llm com controlador, serviço e módulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LlmService } from './llm/llm.service';
+import { LlmController } from './llm/llm.controller';
+import { LlmModule } from './llm/llm.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [LlmModule],
+  controllers: [AppController, LlmController],
+  providers: [AppService, LlmService],
 })
 export class AppModule {}
