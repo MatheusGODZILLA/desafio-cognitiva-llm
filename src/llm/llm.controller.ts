39e59e08a25cb17ccf31914c53ddc1b3059a21c5
Matheus da Silva <contagodzillajapan@gmail.com>
2025-02-14 02:19:32 -0300

    Feat: adicionando controlador com rotas para envio de prompts e geração de texto

--- src/llm/llm.controller.ts
+++ src/llm/llm.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { LlmService } from './llm.service';
+import { PromptDto } from 'src/dto/prompt.dto';
 
 @Controller('llm')
-export class LlmController {}
+export class LlmController {
+  constructor(private readonly llmService: LlmService) {}
+
+  @Get()
+  getHello(): string {
+    return 'Rota de envio de prompts';
+  }
+
+  @Post('generate')
+  async generateText(@Body() promptDto: PromptDto) {
+    return this.llmService.generateFromGemini(promptDto.prompt);
+  }
+}
