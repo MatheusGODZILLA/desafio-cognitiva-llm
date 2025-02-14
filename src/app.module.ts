27c143b926b6ad988693bb8f7b2a18a62592fbe8
Matheus da Silva <contagodzillajapan@gmail.com>
2025-02-14 01:22:06 -0300

    Feat: adicionando serviço Gemini para consumo de API

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { LlmService } from './llm/llm.service';
 import { LlmController } from './llm/llm.controller';
 import { LlmModule } from './llm/llm.module';
+import { GeminiService } from './providers/gemini/gemini.service';
 
 @Module({
   imports: [LlmModule],
   controllers: [AppController, LlmController],
-  providers: [AppService, LlmService],
+  providers: [AppService, LlmService, GeminiService],
 })
 export class AppModule {}
