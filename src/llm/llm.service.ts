f6c3de7cd17d1184a1e9297039b31b66616d1a3d
Matheus da Silva <contagodzillajapan@gmail.com>
2025-02-14 02:18:29 -0300

    Feat: adicionando método para geração de texto

--- src/llm/llm.service.ts
+++ src/llm/llm.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { GeminiService } from '../providers/gemini/gemini.service';
 
 @Injectable()
-export class LlmService {}
+export class LlmService {
+  constructor(private readonly geminiService: GeminiService) {}
+
+  async generateFromGemini(prompt: string) {
+    return this.geminiService.generateText(prompt);
+  }
+}
