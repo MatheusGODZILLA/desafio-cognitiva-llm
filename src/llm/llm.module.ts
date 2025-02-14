9926d5f3b4fc22230f4ce8a6cc2ba9ca4a0a848e
Matheus da Silva <contagodzillajapan@gmail.com>
2025-02-14 02:17:24 -0300

    Feat: adicionando GeminiService ao módulo

--- src/llm/llm.module.ts
+++ src/llm/llm.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LlmService } from './llm.service';
 import { LlmController } from './llm.controller';
+import { GeminiService } from '../providers/gemini/gemini.service';
 
 @Module({
   controllers: [LlmController],
-  providers: [LlmService],
+  providers: [LlmService, GeminiService],
 })
 export class LlmModule {}
